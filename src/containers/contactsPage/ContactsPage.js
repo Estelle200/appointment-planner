473bd39f813a2cc92bde81a803e4fa98b33f7bdb
Esther Ojinji <ojinjiester@gmail.com>
2024-04-14 14:00:36 +0200

    first commit

---
+++ src/containers/contactsPage/ContactsPage.js
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from "react";
+
+import { ContactForm } from "../../components/contactForm/ContactForm";
+import { TileList } from "../../components/tileList/TileList";
+
+export const ContactsPage = (props) => {
+  /*
+  Define state variables for 
+  contact info and duplicate check
+  */
+  const [name, setName] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [email, setEmail] = useState('');
+  const [duplicateName, setDuplicateName] = useState(false);
+
+  
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    /*
+    Add contact info and clear data
+    if the contact name is not a duplicate
+    */
+  };
+
+  /*
+  Using hooks, check for contact name in the 
+  contacts array variable in props
+  */
+
+  return (
+    <div>
+      <section>
+        <h2>Add Contact</h2> 
+      </section>
+      <hr />
+      <section>
+        <h2>Contacts</h2>
+      </section>
+    </div>
+  );
+};
